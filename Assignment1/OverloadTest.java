dffec505b4c038dbeae8dccd23290e4e9334e024
Shlok Kalekar <shlok.kalekar@joshsoftware.com>
2023-07-25 13:41:55 +0530

    Made a small change in Overload Test

--- Assignment1/OverloadTest.java
+++ Assignment1/OverloadTest.java
@@ -1,11 +1,13 @@
 //Write a class with method talk which prints hello and
 //then overload the talk method by taking in a name and printing “Hello” with that name.
+import java.io.*;
+
 public class OverloadTest {
 
     String name;
 
     public String talk(){
-        return "hello";
+        return "hello!";
     }
 
     public String talk(String name){
@@ -13,10 +15,14 @@ public class OverloadTest {
         return "Hello "+name;
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         OverloadTest o1 = new OverloadTest();
+        System.out.println("Enter a name or not: ");
+        String name = br.readLine();
         String greeting = o1.talk();
-        greeting = o1.talk("Shlok");
+        System.out.println(greeting);
+        greeting = o1.talk(name);
         System.out.println(greeting);
     }
 }
